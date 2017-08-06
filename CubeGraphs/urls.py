e915d62fe12cb6cce660fd6381560da17d52c8b4
Chris B <cbrown119955@gmail.com>
2017-08-06 14:57:51 -0700

    Add navbar

--- CubeGraphs/urls.py
+++ CubeGraphs/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^about/', include('about.urls')),
     url(r'', include('home.urls')),
 ]
--- about/views.py
+++ about/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'home/home.html', {})
+    return render(request, 'about/about.html', {})

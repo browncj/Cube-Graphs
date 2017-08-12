3bdfc94a7a76f144db77c65cae889143a6137b47
Chris B <cbrown119955@gmail.com>
2017-08-11 20:27:50 -0700

    Timer starts and stops with buttons

--- static/timer/scripts.js
+++ static/timer/scripts.js
@@ -1,8 +1,46 @@
+var clock = null;
+var count = 0;
+
+function setTime(total_centiseconds){
+    var centiseconds = total_centiseconds % 100;
+    var seconds = Math.floor(total_centiseconds / 100) % 60;
+    var minutes = Math.floor(total_centiseconds / 6000);
+
+    if (centiseconds < 10){
+        centiseconds = '0' + centiseconds;
+    }
+
+    if (seconds < 10){
+        seconds = '0' + seconds;
+    }
+
+    if (minutes < 10) {
+        minutes = '0' + minutes;
+    }
+
+    $('#minutes').text(minutes);
+    $('#seconds').text(seconds);
+    $('#centiseconds').text(centiseconds);
+}
+
 $(document).ready(function(){
     $('#timer-page').toggleClass('active');
+    setTime(count);
 
-    // TODO: Countdown logic
-    $('#minutes').text('03');
-    $('#seconds').text('24');
-    $('#centiseconds').text('92');
+    $('.btn#start').click(function(){
+        clock = setInterval(function(){
+            count = count + 1;
+            setTime(count);
+        }, 10);
+    });
+
+    $('.btn#stop').click(function() {
+        clearInterval(clock);
+    });
+
+    $('.btn#reset').click(function(){
+        clearInterval(clock);
+        count = 0;
+        setTime(count);
+    });
 });

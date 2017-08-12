a0bbb0a71cc0627902efc4598f2a906c585c5252
Chris B <cbrown119955@gmail.com>
2017-08-11 21:20:47 -0700

    Integrate with postgresql

--- CubeGraphs/settings.py
+++ CubeGraphs/settings.py
@@ -77,10 +77,21 @@ WSGI_APPLICATION = 'CubeGraphs.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'test1',
+        'USER': 'test1',
+        'PASSWORD': 'welcome123',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation

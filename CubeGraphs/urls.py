e71ee0a013ab2c88468ec54e38ede7a20000c57a
Chris B <cbrown119955@gmail.com>
2017-08-13 15:38:48 -0700

    Support user registration and login

--- CubeGraphs/urls.py
+++ CubeGraphs/urls.py
@@ -15,11 +15,18 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
+from CubeGraphs import views as top_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', top_views.base, name='base'),
     url(r'^about/', include('about.urls')),
     url(r'^timer/', include('timer.urls')),
-    url(r'', include('home.urls')),
+    url(r'^home/', include('home.urls')),
+
+    url(r'^login/', auth_views.login, name='login'),
+    url(r'^logout/', auth_views.logout, {'next_page': '/'}, name='logout'),
+    url(r'^signup/', top_views.signup, name='signup'),
 ]
---
+++ static/registration/signup.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+    $('#signup-page').toggleClass('active');
+})

1a5c11289f086f577510242320b45177fca5efb8
Chris B <cbrown119955@gmail.com>
2017-08-30 12:48:54 -0700

    Add update user email functionality

--- userProfile/urls.py
+++ userProfile/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='Profile')
+    url(r'^$', views.index, name='Profile'),
+    url(r'^update_mail/', views.update_mail, name='Add Mail'),
 ]
--- userProfile/views.py
+++ userProfile/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.core.validators import validate_email
 
 
 def index(request):
@@ -6,3 +7,13 @@ def index(request):
         return redirect('/login')
 
     return render(request, 'userProfile/userProfile.html', {})
+
+
+def update_mail(request):
+    if request.method == 'POST':
+        validate_email(request.POST['email'])
+        request.user.email = request.POST['email']
+        request.user.save()
+        return redirect('/profile')
+
+    return render(request, 'userProfile/updateMail.html', {})

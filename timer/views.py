fe44b8a6c073bbc640ba6d1720675f52a3c7daef
Chris B <cbrown119955@gmail.com>
2017-09-08 18:21:52 -0700

    Add view decorator for POST

--- timer/views.py
+++ timer/views.py
@@ -1,11 +1,15 @@
+from django.views.decorators.http import require_POST
 from django.shortcuts import render, HttpResponse
+from django.http import JsonResponse
+
+from timer.models import Solve
 
 
 def index(request):
     return render(request, 'timer/timer.html', {})
 
+
+@require_POST
 def submit(request):
-    if request.method == 'POST':
-        return HttpResponse('I got a post')
-    else:
-        return HttpResponse('something')
+    if request.is_ajax():
+        return JsonResponse({'data': 3.14})

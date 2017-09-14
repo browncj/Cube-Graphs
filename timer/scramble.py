04860730395fa5ceeff601283c0de4cbf0bee1e0
Chris B <cbrown119955@gmail.com>
2017-09-14 16:48:10 -0700

    Create real scrambles

--- timer/scramble.py
+++ timer/scramble.py
@@ -4,4 +4,12 @@ import random
 
 
 def scramble():
-    return str(random.randint(0, 99999))
+    candidates = ["R", "R'", "R2", "L", "L'", "L2",
+                    "U", "U'", "U2", "D", "D'", "D2",
+                    "F", "F'", "F2", "B", "B'", "B2"]
+    scramble = ''
+    for _ in range(20):
+        scramble += random.choice(candidates)
+        if _ != 14:
+            scramble += ' '
+    return scramble
